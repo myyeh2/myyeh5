433de78f72e21682fb1332af818ebf68e9462ad6
myyeh2 <myyeh2@xuite.net>
2023-02-01 15:59:49 +0800

    second-project

---
+++ pyTest2.py
@@ -0,0 +1,27 @@
+# C:\GitHub\test2\pyText2.py
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+fs = 100 
+t = np.arange(0, 1, 1/fs) 
+y = np.cos( 2 * np.pi * 10 * t) 
+
+plt.figure(figsize = [10, 5]) 
+plt.plot(t, y) 
+plt.title('Signal plot') 
+# plt.show() 
+
+yf = np.fft.fft(y) 
+w_hat = np.arange(0, 2 * np.pi, np.pi * 2 / fs) 
+
+plt.figure(figsize=[15, 5]) 
+plt.subplot(121) 
+plt.plot(w_hat, abs(yf)) 
+plt.xlabel(r'$\hat\omega$ (radian)') 
+plt.title('The magnitude spectrum') 
+plt.subplot(122) 
+plt.plot(np.angle(yf)) 
+plt.xlabel(r'$\hat\omega$ (radian)') 
+plt.title('The phase spectrum') 
+plt.show()  
